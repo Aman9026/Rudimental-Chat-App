40538ca34685c226fc4af628b10437d18bab0800
Aman9026 <amangupta884036@gmail.com>
2020-09-16 23:51:07 +0530

    Complete homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import '';
+import 'chatscreen.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(
-        title: new Text("ChatApp"),
-      ),
-      body: new ChatScreen(),
-    );
+        appBar: AppBar(
+          title: new Text("ChatApp"),
+        ),
+        body: new ChatScreen());
   }
 }
